6de7a3ee126233dc0f468fb581415e2d8cfc0c7a
phollakrit <66252619+phollakrit@users.noreply.github.com>
2024-02-28 16:48:49 +0700

    Work9 Finished
    Work9 Finished

--- register.php
+++ register.php
@@ -9,9 +9,9 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
-    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <title>Register</title>
 </head>
 <body>
@@ -21,49 +21,61 @@
         <div class="row mt-4">
             <div class="col-lg-3 col-md-2 col-sm-1"></div>
             <div class="col-lg-6 col-md-8 col-sm-10">
+                <?php
+                    if(isset($_SESSION['add_login'])){
+                        if($_SESSION['add_login']=="error"){
+                            echo "<div class='alert alert-danger'>
+                            ชื่อบัญชีซ้ำหรือฐานข้อมูลมีปัญหา</div>";
+                        }else{
+                            echo "<div class='alert alert-success'>
+                            เพิ่มบัญชีเรียบร้อยแล้ว</div>";
+                        }
+                        unset($_SESSION['add_login']);
+                    }
+                ?>
                 <div class="card border-primary">
                     <div class="card-header bg-primary text-white">เข้าสู่ระบบ</div>
                     <div class="card-body">
                         <form action="register_save.php" method="post">
                             <div class="row">
-                                <label for="user" class="col-lg-3 col-form-label">ชื่อบัญชี:</label>
+                                <label class="col-lg-3 col-form-label">ชื่อบัญชี:</label>
                                 <div class="col-lg-9">
-                                    <input id="user" type="text" name="login" class="form-control" required>
+                                    <input type="text" name="login" class="form-control" required>
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <label for="pwd" class="col-lg-3 col-form-label">รหัสผ่าน:</label>
+                                <label class="col-lg-3 col-form-label">รหัสผ่าน:</label>
                                 <div class="col-lg-9">
-                                    <input id="pwd" type="password" name="pwd" class="form-control" required>
+                                    <input type="password" name="pwd" class="form-control" required>
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <label for="name" class="col-lg-3 col-form-label">ชื่อ-นามสกุล:</label>
+                                <label class="col-lg-3 col-form-label">ชื่อ-นามสกุล:</label>
                                 <div class="col-lg-9">
-                                    <input id="name" type="text" name="name" class="form-control" required>
+                                    <input type="text" name="name" class="form-control" required>
                                 </div>
                             </div>
                             <div class="row mt-3">
                                 <label class="col-lg-3 form-label">เพศ:</label>
                                 <div class="col-lg-9">
                                     <div class="form-check">
-                                        <input id="m" type="radio" name="gender" value="m" class="form-check-input" required>
-                                        <label for="m" class="form-check-label">ชาย</label>
+                                        <input type="radio" name="gender" value="m" class="form-check-input" required>
+                                        <label class="form-check-label">ชาย</label>
                                     </div>
                                     <div class="form-check">
-                                        <input id="f" type="radio" name="gender" value="f" class="form-check-input" required>
-                                        <label for="f" class="form-check-label">หญิง</label>
+                                        <input type="radio" name="gender" value="f" class="form-check-input" required>
+                                        <label class="form-check-label">หญิง</label>
                                     </div>
                                     <div class="form-check">
-                                        <input id="o" type="radio" name="gender" value="o" class="form-check-input" required>
-                                        <label for="o" class="form-check-label">อื่นๆ</label>
+                                        <input type="radio" name="gender" value="o" class="form-check-input" required>
+                                        <label class="form-check-label">อื่นๆ</label>
                                     </div>
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <label for="mail" class="col-lg-3 col-form-label">อีเมล:</label>
+                                <label class="col-lg-3 col-form-label">อีเมล:</label>
                                 <div class="col-lg-9">
-                                    <input id="mail" type="email" name="email" class="form-control" required>
+                                    <input type="email" name="email" class="form-control" required>
                                 </div>
                             </div>
                             <div class="row mt-3">
@@ -80,7 +92,7 @@
             <div class="col-lg-3 col-md-2 col-sm-1"></div>
         </div>
     </div>
-    <br>   
+    <br>    
 </body>
 </html>
 
